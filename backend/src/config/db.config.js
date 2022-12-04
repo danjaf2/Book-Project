b51bb869db873d5b5cf8c777866f4085f030bdaa
Timmy-Pierre Keo <timmypierre.keo@gmail.com>
2022-12-04 18:58:56 -0500

    Using jest instead

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76ad1fbc46f598b25ff35230c19a508729a595d93243490e08fcbba83c74adde
-size 536
+oid sha256:c8f302c5731b716b2612f64221a0bf08f548f672ffe5e105df217bba14a7768a
+size 557
--- backend/tests/AllShelvesRoute.test.js
+++ backend/tests/AllShelvesRoute.test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26671c2b67d4b8ffd8915b3e49eca6149b68a38fa8b0f7ca4e705babf92d2398
-size 1470
+oid sha256:df934885f183c0a1d67885d7d3ca35b80bfa9490f342ac98f654b5ce37c879b5
+size 1324
